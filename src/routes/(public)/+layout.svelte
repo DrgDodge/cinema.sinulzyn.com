6165cee95c32cb20a90d9d4c7f5013a79395d3ff
DrgDodge <dragonul530@gmail.com>
2026-06-12 02:25:45 +0300

    V0.3.7 - Move layouts

---
+++ src/routes/(public)/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import '../layout.css';
+    let { children } = $props();
+</script>
+
+<div class="min-h-screen bg-gray-950 text-gray-100 flex flex-col font-sans antialiased">
+    {@render children()}
+</div>
